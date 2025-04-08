a625b344df7a10b486eece28d313d13013eca5d7
trololo007 <ryzhkova.ann@dvfu.ru>
2025-04-08 11:22:04 +1000

    3 задание

---
+++ krr_z3.pas
@@ -0,0 +1,12 @@
+﻿procedure DeletingUnit(var head: Plist);
+var 
+  current: PList;
+  begin
+    current := head;
+    while current <> nil do
+      begin
+        if (current^.numbers mod 2 <> 0) then
+          current^.numbers := current^.numbers - 1;
+        current := current^.next;
+      end;
+  end;
